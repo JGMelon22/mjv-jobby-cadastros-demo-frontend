f2da33d7adae36f020abad3ad1970534c96e7aa7
JGMelon22 <jgvasconcellos22@gmail.com>
2023-07-08 15:34:58 -0300

    Montando o esqueleto inicial da tabela e realizando os imports do axios e reactstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
-import './App.css'
+import './App.css';
+import axios from 'axios';
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap'
+import logoCadastro from './assets/cadastro.png'
+
 
 function App() {
+
   return (
     <>
-      <h1 className='bg-danger'>Hello, React and Vite!</h1>
-      <button className='btn btn-dark '>Click me!</button>
+      <br />
+      <h3>Cadastro de Candidatos</h3>
+      <header>
+        <img src={logoCadastro} alt='Cadastro' />
+        <button className='btn btn-success'>Incluir Novo Candidato</button>
+      </header>
+      <table className='table table-bordered table-hover'>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Cpf</th>
+            <th>Data Nascimento</th>
+            <th>Sexo</th>
+            <th>Estado</th>
+            <th>Profissão</th>
+            <th>Operação</th> {/* Incluir botão de detalhes do candidato p/ listar todas as suas informações */}
+          </tr>
+        </thead>
+        <tbody>
+          {/*TODO*/}
+        </tbody>
+      </table>
     </>
   )
 }
