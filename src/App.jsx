438ecfb627baefc475ee4d3476a01d99abc7cc9f
JGMelon22 <jgvasconcellos22@gmail.com>
2023-07-09 12:42:40 -0300

    Desenvolvendo a janela modal para edição de um candidato e sua lógica de abrir e fechar assim como uma breve melhoria na estética do tema

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,8 @@ function App() {
   // Estado da janela modal de inserção de dados
   const [modalIncluir, setModalIncluir] = useState(false);
 
+  const [modalEditar, setModalEditar] = useState(false);
+
   // Cria o estado candidatoSelecionado
   const [cadastroSelecionado, setCadastroSelecionado] = useState(
     {
@@ -49,6 +51,10 @@ function App() {
     setModalIncluir(!modalIncluir);
   }
 
+  const abrirFecharModalEditar = () => {
+    setModalEditar(!modalEditar);
+  }
+
   // handleChange para guardar os dados do candidato
   // que será informado nos inputs
   // e o setCandidatoSelecionado para atualizar o estado
@@ -58,7 +64,6 @@ function App() {
       ...cadastroSelecionado,
       [name]: type === 'checkbox' ? (checked ? 1 : 0) : value // Tratativa para componentes que são checkbox)
     });
-    console.log(cadastroSelecionado);
   }
 
   // Requisição Get com o axios
@@ -77,6 +82,7 @@ function App() {
     cadastroSelecionado.salario = parseFloat(cadastroSelecionado.salario);
     cadastroSelecionado.pretencaoMinima = parseFloat(cadastroSelecionado.pretencaoMinima);
     cadastroSelecionado.pretencaoMaxima = parseFloat(cadastroSelecionado.pretencaoMaxima);
+
     await axios.post(baseUrl, cadastroSelecionado)
       .then(response => {
         setData(data.concat(response.data));
@@ -93,11 +99,13 @@ function App() {
 
   return (
     <>
-      <br />
-      <h3>Jobby Demo 01</h3>
-      <header id='HeaderId'>
-        <img id='AddCadastroLogo' src={logoCadastro} alt='Cadastro' />
-        <button className='btn btn-success' onClick={() => abrirFecharModalIncluir()}>Incluir Novo Candidato</button>
+      <nav className="navbar navbar-expand-lg opacity-75 navbar-dark bg-primary">
+        <a className="navbar-brand m-1 border rounded" href='http://localhost:5173/'>&nbsp;Jobby Demo 01&nbsp;</a>
+        <ul className="navbar-nav me-auto" />
+      </nav>
+      <header id="CreateHeader">
+        <img id="AddCadastroLogo" src={logoCadastro} alt='Adicionar' />
+        <button className='btn btn-success' onClick={() => abrirFecharModalIncluir()}>Adicionar Novo Candidato</button>
       </header>
       <table className='table table-bordered table-hover'>
         <thead>
@@ -109,7 +117,7 @@ function App() {
             <th>Sexo</th>
             <th>Estado</th>
             <th>Profissão</th>
-            <th>Operação</th> {/* Incluir botão de detalhes do candidato p/ listar todas as suas informações */}
+            <th className='text-center'>Operação</th> {/* Incluir botão de detalhes do candidato p/ listar todas as suas informações */}
           </tr>
         </thead>
         <tbody>
@@ -124,7 +132,7 @@ function App() {
               <td>{cadastro.profissao}</td>
               <td className='btn-group'>
                 <button className='btn btn-info text-white rounded m-1'>Detalhes</button>
-                <button className='btn btn-primary rounded m-1'>Editar</button>
+                <button className='btn btn-primary rounded m-1' onClick={() => abrirFecharModalEditar()}>Editar</button>
                 <button className='btn btn-danger rounded m-1'>Deletar</button>
               </td>
             </tr>)}
@@ -147,7 +155,7 @@ function App() {
             <input type='text' className='form-control' name='dataNascimento' onChange={handleChange}></input>
             <label>Sexo</label>
             <br />
-            <input type='text' size="1" className='form-control' name='sexo' onChange={handleChange}></input>
+            <input type='text' className='form-control' name='sexo' onChange={handleChange}></input>
             <label>Logradouro</label>
             <br />
             <input type='text' className='form-control' name='logradouro' onChange={handleChange}></input>
@@ -210,6 +218,87 @@ function App() {
         </ModalFooter>
       </Modal>
 
+      {/* Janela modal para edição de um candidato */}
+      <Modal isOpen={modalEditar}>
+        <ModalHeader>Editar informações</ModalHeader>
+        <ModalBody className='form-group'>
+          <label>Id</label>
+          <br />
+          <input type='text' className='form-control' name='idCadastro' value={cadastroSelecionado && cadastroSelecionado.idCadastro} readOnly></input>
+          <div className='form-group'>
+            <label>Nome</label>
+            <br />
+            <input type='text' className='form-control' name='nome' onChange={handleChange}></input>
+            <label>CPF</label>
+            <br />
+            <input type='number' className='form-control' name='cpf' onChange={handleChange}></input>
+            <label>Data Nascimento</label>
+            <br />
+            <input type='text' className='form-control' name='dataNascimento' onChange={handleChange}></input>
+            <label>Sexo</label>
+            <br />
+            <input type='text' className='form-control' name='sexo' onChange={handleChange}></input>
+            <label>Logradouro</label>
+            <br />
+            <input type='text' className='form-control' name='logradouro' onChange={handleChange}></input>
+            <label>Numero</label>
+            <br />
+            <input type='number' className='form-control' name='numero' onChange={handleChange}></input>
+            <label>Bairro</label>
+            <br />
+            <input type='text' className='form-control' name='bairro' onChange={handleChange}></input>
+            <label>Complemento</label>
+            <br />
+            <input type='text' className='form-control' name='complemento' onChange={handleChange}></input>
+            <label>Cidade</label>
+            <br />
+            <input type='text' className='form-control' name='cidade' onChange={handleChange}></input>
+            <label>Estado</label>
+            <br />
+            <input type='text' size="2" min="2" max="2" className='form-control' name='estado' onChange={handleChange}></input>
+            <label>E-mail</label>
+            <br />
+            <input type='email' className='form-control' name='email' onChange={handleChange}></input>
+            <label>Telefone</label>
+            <br />
+            <input type='tel' className='form-control' name='telefone' onChange={handleChange}></input>
+            <label>Celular</label>
+            <br />
+            <input type='tel' className='form-control' name='celular' onChange={handleChange}></input>
+            <label>WhatsApp?&nbsp;</label>
+            <input type='checkbox' className='form-check-input' name='celularWhats' onChange={handleChange}></input>
+            <br />
+            <br />
+            <label>Profissão</label>
+            <br />
+            <input type='text' className='form-control' name='profissao' onChange={handleChange}></input>
+            <label>Empresa</label>
+            <br />
+            <input type='text' className='form-control' name='empresa' onChange={handleChange}></input>
+            <label>Salário</label>
+            <br />
+            <input type='number' className='form-control' name='salario' onChange={handleChange}></input>
+            <label>Emprego Atual?&nbsp;</label>
+            <input type='checkbox' className='form-check-input' name='empregoAtual' onChange={handleChange}></input>
+            <br />
+            <br />
+            <label>Pretenção Salarial Mínima</label>
+            <br />
+            <input type='number' className='form-control' name='pretencaoMinima' onChange={handleChange}></input>
+            <label>Pretenção Salarial Máxima</label>
+            <br />
+            <input type='number' className='form-control' name='pretencaoMaxima' onChange={handleChange}></input>
+            <label>Habilidades</label>
+            <br />
+            <input type='text' className='form-control' name='habilidades' onChange={handleChange}></input>
+          </div>
+        </ModalBody>
+
+        <ModalFooter>
+          <button className='btn btn-primary m-1' onClick={() => putCandidato()}>Editar</button> {" "}
+          <button className='btn btn-danger m-1' onClick={() => abrirFecharModalEditar()}>Cancelar</button>
+        </ModalFooter>
+      </Modal>
     </>
   )
 }
